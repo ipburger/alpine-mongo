a714642da356fc364dc93bfa6b9e047d23a161a5
Vitor Araujo Boufleur <v.boufleur@gmail.com>
2018-02-03 18:31:18 -0300

    chown after repair: mongod repair leave files owned by root behind

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 # Docker entrypoint (pid 1), run as root
 [ "$1" = "mongod" ] || exec "$@" || exit $?
 
-# Make sure that database is owned by user mongodb
-[ "$(stat -c %U /data/db)" = mongodb ] || chown -R mongodb /data/db
-
 #Repair the database before anything
 mongod --repair
+
+# Make sure that database is owned by user mongodb
+chown -R mongodb /data/db
 
 # Drop root privilege (no way back), exec provided command as user mongodb
 cmd=exec; for i; do cmd="$cmd '$i'"; done
